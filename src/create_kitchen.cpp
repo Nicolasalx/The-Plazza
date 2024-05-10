0c85ac51d2ee31493cebf1a513236caca59c3a68
thibaudcathala <thibaud.cathala@epitech.eu>
2024-05-10 12:05:24 +0200

    fix: issue when launch multiple kitchen rely fast, the key where the same

--- src/create_kitchen.cpp
+++ src/create_kitchen.cpp
@@ -15,6 +15,8 @@ void Pla::Reception::createKitchen()
 {
     this->mutex_.lock();
     this->kitchen_list_.emplace_back();
+    key_t send_key = this->kitchen_list_.back().recv_msg_queue.getKey();
+    key_t recv_key = this->kitchen_list_.back().send_msg_queue.getKey();
     this->mutex_.unlock();
 
     pid_t pid = fork();
@@ -23,15 +25,10 @@ void Pla::Reception::createKitchen()
         throw my::tracked_exception("Failed to fork.");
     } else if (pid == 0) {
         Pla::Kitchen kitchen(
-            nb_cook_,
-            cooking_time_,
-            ing_repl_time_,
-            this->kitchen_list_.back().recv_msg_queue.getKey(),
-            this->kitchen_list_.back().send_msg_queue.getKey()
+            nb_cook_, cooking_time_, ing_repl_time_, send_key, recv_key
         );
         std::exit(0);
     }
-
     this->mutex_.lock();
     std::cout << "\e[92mKitchen " << pid << " created !\e[0m" << std::endl;
     this->kitchen_list_.back().pid = pid;
@@ -42,14 +39,12 @@ void Pla::Reception::closeAllKitchen()
 {
     int status = 0;
     pid_t pid = 0;
+    Pla::Message msg;
 
     while (!this->kitchen_list_.empty()) {
         pid = waitpid(this->kitchen_list_.back().pid, &status, WNOHANG);
         if (pid == 0) {
-            //kill(pid, SIGKILL);
             this->kitchen_list_.back().send_msg_queue.push(Pla::Message(Pla::MessageType::CLOSE_KITCHEN));
-
-            Pla::Message msg;
             do {
                 this->kitchen_list_.back().recv_msg_queue.pop(msg);
             } while (msg.getType() != Pla::MessageType::CLOSE_KITCHEN);
