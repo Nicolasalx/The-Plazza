95bc7a5425d1e51041cc9509f4d54fa82d7a7fc4
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-05-13 17:23:42 +0200

    mod: system to handle the reception of the pizza in the parsing of the command has been changed

--- src/reception.cpp
+++ src/reception.cpp
@@ -10,6 +10,7 @@
 #include "Kitchen.hpp"
 #include "my_tracked_exception.hpp"
 #include <iostream>
+#include "ParseCommand.hpp"
 
 Pla::Reception::Reception(int argc, const char **argv) : exit_(false)
 {
@@ -33,25 +34,42 @@ Pla::Reception::Reception(int argc, const char **argv) : exit_(false)
 
 void Pla::Reception::handleInput(const std::string &input)
 {
-    Pla::Order new_order;
-
-    std::cerr << "Get: \"" << input << "\"\n";
-
-    //    TODO: - parse the input
+    Pla::ParseCommand parseCmd;
+    std::vector<Pla::Order> allOrder;
 
     if (input == "status") {
         displayStatus();
     } else {
-        this->mutex_.lock();
-        // ! Data to fill with parsing :
-        new_order.nb = this->order_index_++;
-        new_order.type = Pla::PizzaType::Regina;
-        new_order.size = Pla::PizzaSize::L;
-        new_order.state = Pla::PizzaState::WAITING_TO_BE_COOK;
-        // ! ---------------------------
-        order_.push(new_order);
-        this->mutex_.unlock();
+        allOrder = parseCmd.parsingCommand(input, this->order_index_);
+        if (allOrder.empty()) {
+            return;
+        }
+        for (const auto &ord: allOrder) {
+            this->mutex_.lock();
+            order_.push(ord);
+            this->mutex_.unlock();
+        }
     }
+
+    // Pla::Order new_order;
+// 
+    // std::cerr << "Get: \"" << input << "\"\n";
+// 
+    // //    TODO: - parse the input
+// 
+    // if (input == "status") {
+    //     displayStatus();
+    // } else {
+    //     this->mutex_.lock();
+    //     // ! Data to fill with parsing :
+    //     new_order.nb = this->order_index_++;
+    //     new_order.type = Pla::PizzaType::Regina;
+    //     new_order.size = Pla::PizzaSize::L;
+    //     new_order.state = Pla::PizzaState::WAITING_TO_BE_COOK;
+    //     // ! ---------------------------
+    //     order_.push(new_order);
+    //     this->mutex_.unlock();
+    // }
 }
 
 void Pla::Reception::openPlazza()
